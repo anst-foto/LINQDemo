ae143aa08e11cff94c17c2f288bff22a81335f8e
Starinin Andrey <starininandrey@gmail.com>
2023-09-02 13:19:30 +0300

    Update

---
+++ LINQDemo/Person.cs
@@ -0,0 +1,37 @@
+﻿namespace LINQDemo;
+
+/*
+public class Person : IEquatable<Person>
+{
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public IEnumerable<string> Phones { get; init; }
+
+    public bool Equals(Person? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return FirstName == other.FirstName && LastName == other.LastName && Phones.Equals(other.Phones);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Person)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(FirstName, LastName, Phones);
+    }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName} {Phones}";
+    }
+}
+*/
+
+public record Person(string FirstName, string LastName, IEnumerable<string> Phones);
--- LINQDemo/Program.cs
+++ LINQDemo/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LINQDemo;
+
+var persons = new List<Person>()
+{
+    new(LastName: "Starinin",
+        FirstName: "Andrey",
+        Phones: new List<string>{"+79042144491", "+74732575755"}),
+    new(LastName: "Anonimus",
+        FirstName: "Anonim",
+        Phones: Array.Empty<string>())
+};
+
+var personsWithNotPhone = from person in persons
+    where person.Phones.Count() == 0
+    select new {FullName = $"{person.LastName} {person.FirstName}"};
+foreach (var item in personsWithNotPhone)
+{
+    Console.WriteLine(item.FullName);
+}
+
+var result = from person1 in persons
+    from phone in person1.Phones
+    let number = "904"
+    where phone.Contains(number)
+    select (person1.LastName, phone);
+foreach (var (lastName, phone) in result)
+{
+    Console.WriteLine($"{lastName} -> {phone}");
+}
+
+return;
